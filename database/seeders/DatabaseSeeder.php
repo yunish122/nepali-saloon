dbb793e46f8cf37ad12792b2f31d543d50535604
Yunish Manandhar <manandharunish1@gmail.com>
2026-02-06 17:41:51 +0545

    Seeder seeded

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,9 +3,12 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Database\Seeders\Invarient\RoleSeeder as InvarientRoleSeeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Database\Seeders\Invarient\RoleSeeder;
+use Database\Seeders\Invarient\PermissionSeeders;
+use Database\Seeders\Invarient\RolesPermissionSeeders;
 class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
@@ -16,10 +19,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            RoleSeeder::class,
+            PermissionSeeders::class,
+            RolesPermissionSeeders::class
         ]);
     }
 }
